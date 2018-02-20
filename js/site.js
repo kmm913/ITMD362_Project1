324620b4b881dac75719af5738907dd4f571af32
kmm913 <kmahoney@hawk.iit.edu>
2018-02-20 16:36:33 -0600

    fixed js confirmSubmission() function
    before it was letting people "submit" a blank form. Now it won't.

--- js/site.js
+++ js/site.js
@@ -9,7 +9,14 @@ jQuery(function($) {
 document.getElementById("submit").addEventListener("click", confirmSubmission); 
 
 function confirmSubmission(event){
-  alert("Thank you! Your form has been submitted."); 
+  for(i = 0; i < document.getElementsByTagName('input').length; i++){
+    if(document.getElementsByTagName('input').item(i).value == "" ||
+        (document.getElementsByTagName('input').item(i).type == "checkbox" && document.getElementsByTagName('input').item(i).checked==false)){
+          return false; 
+        }   
+  }
+  alert("Thank you! Your form has been submitted.");
+  return true; 
 }
 
 /*
