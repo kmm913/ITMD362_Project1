7a6fbf4e7f3ee5a289960f6687ddccbcaaf169ad
kmm913 <kmahoney@hawk.iit.edu>
2018-02-20 14:22:36 -0600

    added validation for checkbox

--- js/site.js
+++ js/site.js
@@ -8,7 +8,8 @@ document.getElementById("submit").addEventListener("click", validate);
 
 function validate(event){
   for(i = 0; i < document.getElementsByTagName('input').length; i++){
-    if(document.getElementsByTagName('input').item(i).value == ""){
+    if(document.getElementsByTagName('input').item(i).value == "" ||
+        (document.getElementsByTagName('input').item(i).type == "checkbox" && document.getElementsByTagName('input').item(i).checked==false)){
       alert("Please enter your " + document.getElementsByTagName('input').item(i).name);
       event.preventDefault();
       return false;
